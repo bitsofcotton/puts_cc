d4da5e4144a9e0a8c858dd516ffc0d38f09a9a30
bitsofcotton <kazunobu@mail.limpid-intensity.info>
2019-07-10 16:40:23 +0900

    dictation speed up.

--- prep.py
+++ prep.py
@@ -8,15 +8,15 @@ sys.setdefaultencoding("utf-8")
 
 import MySQLdb
 
-conn = MySQLdb.connect(user='root', passwd='', host='127.0.0.1', db='wiktionary', charset='utf8')
+conn  = MySQLdb.connect(user='root', passwd='', host='127.0.0.1', db='wiktionary', charset='utf8')
+conn2 = MySQLdb.connect(user='root', passwd='', host='127.0.0.1', db='wiktionary', charset='utf8')
 cur  = conn.cursor()
+cur2 = conn2.cursor()
 for word in sys.argv[2:]:
   cur.execute("select page_latest from page where page_title=%s", [word])
   res  = ""
   for s in range(0, 20):
     try:
-      conn2 = MySQLdb.connect(user='root', passwd='', host='127.0.0.1', db='wiktionary', charset='utf8')
-      cur2 = conn2.cursor()
       work = cur.fetchone()[0]
       cur2.execute("select old_text from text where old_id=%s", [work])
       res += cur2.fetchone()[0]
